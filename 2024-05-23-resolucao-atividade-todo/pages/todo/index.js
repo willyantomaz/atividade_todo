e0b7e585df8ca9fdf651b8e50a3091f7c191455d
Willyan Tomaz <willyantomaz2015@gmail.com>
2024-05-24 01:25:59 +0000

    1

--- 2024-05-23-resolucao-atividade-todo/pages/todo/index.js
+++ 2024-05-23-resolucao-atividade-todo/pages/todo/index.js
@@ -13,18 +13,23 @@ function addTask(event) {
   const taskDescription = formData.get('description')
 
   const li = document.createElement('li')
+  
+  const dialog = document.getElementById('favDialog');
+
+ 
 
   li.id = taskId
   li.innerHTML = `
       <h2>${taskTitle}</h2>
       <p>${taskDescription}</p>
-  `
+      <p><button class="edita" title="Ediar tarefa" >✏️</button></p>
 
+  `
   taskList.appendChild(li)
 
   // Salvar tarefas no localStorage
   const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
-  tasks.push({ title: taskTitle, description: taskDescription })
+  tasks.push({ title: taskTitle, description: taskDescription, buton: '✏️' })
   localStorage.setItem(taskKey, JSON.stringify(tasks))
 
   form.reset()
@@ -35,6 +40,10 @@ window.addEventListener('DOMContentLoaded', () => {
   const tasks = JSON.parse(localStorage.getItem(taskKey)) || []
   const taskList = document.querySelector('#taskList')
   taskList.innerHTML = tasks
-    .map((task) => `<li><h2>${task.title}</h2><p>${task.description}</p></li>`)
+    .map((task) => `<li><p><button class="edita" title="Ediar tarefa">✏️</button></p><h2>${task.title}</h2><p>${task.description}</p> </li>`)
     .join('')
+})
+
+edita.addEventListener('click', function() {
+  dialog.showmodal()
 })
